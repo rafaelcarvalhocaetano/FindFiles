091a6dd5b6ea85fb46620b4fb1272d4d3a069374
rafael <rapha.pse@outlook.com>
2020-06-05 23:07:09 -0300

    Finished Home

--- lib/main.dart
+++ lib/main.dart
@@ -33,110 +33,124 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-//        scrollDirection: Axis.horizontal,
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.only(left: 15.0, right: 15.0),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(left: 10.0, top: 50.0),
-                  child: Row(
+      body: Container(
+        color: Colors.white,
+        child: CustomScrollView(
+          slivers: <Widget>[
+            SliverAppBar(
+              backgroundColor: Colors.white,
+              expandedHeight: 220,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 55.0),
+                  child: Column(
                     children: <Widget>[
-                      Text('Find', style: TextStyle(
-                          fontSize: 32,
-                          fontWeight: FontWeight.bold
-                      ),),
-                      SizedBox(width: 10,),
-                      Text('your files', style: TextStyle(
-                          fontWeight: FontWeight.w400,
-                          fontSize: 24
-                      ),),
-                      Spacer(),
-                      IconButton(
-                        icon: Icon(
-                          Icons.add,
-                          color: Colors.black,
-                        ),
-                        onPressed: () { print('A1'); },
+                      Row(
+                        children: <Widget>[
+                          Text('Find', style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold
+                          ),),
+                          SizedBox(width: 10,),
+                          Text('your files', style: TextStyle(
+                              fontWeight: FontWeight.w400,
+                              fontSize: 24
+                          ),),
+                          Spacer(),
+                          IconButton(
+                            icon: Icon(
+                              Icons.add,
+                              color: Colors.black,
+                            ),
+                            onPressed: () { print('A1'); },
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.folder_open,
+                              color: Colors.black,
+                            ),
+                            onPressed: () { print('A2'); },
+                          ),
+                        ],
                       ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.folder_open,
-                          color: Colors.black,
+//                      SizedBox(height: 20,),
+                      Container(
+                        padding: const EdgeInsets.only(top: 15.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search o files',
+                            hintStyle: TextStyle(color: Colors.black),
+                            contentPadding: EdgeInsets.all(20),
+                            focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                              borderSide: BorderSide(color: Colors.transparent, width: 1),
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            filled: true,
+                            fillColor: Color(0xFFF6F5FB),
+                            suffixIcon: Icon(Icons.search, color: Colors.black,),
+                          ),
                         ),
-                        onPressed: () { print('A2'); },
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          IconButton(icon: Icon(Icons.border_all), onPressed: () { print('BORDER 1 '); }, ),
+                          IconButton(icon: Icon(Icons.border_vertical), onPressed: () { print('BORDER 1 '); }, ),
+                        ],
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 10,),
-                Container(
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: 'Search o files',
-                      hintStyle: TextStyle(color: Colors.black),
-                      contentPadding: EdgeInsets.all(20),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                        borderSide: BorderSide(color: Colors.transparent, width: 1),
-                      ),
-
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
-                      filled: true,
-                      fillColor: Colors.grey[200],
-                      suffixIcon: Icon(Icons.search, color: Colors.black,),
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    IconButton(icon: Icon(Icons.border_all), onPressed: () { print('BORDER 1 '); }, ),
-                    IconButton(icon: Icon(Icons.border_vertical), onPressed: () { print('BORDER 1 '); }, ),
-                  ],
-                ),
-                Container(
-                  height: MediaQuery.of(context).size.height,
-                  child: new GridView.count(
+              ),
+            ),
+            SliverGrid(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 1,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 1,
+                childAspectRatio:MediaQuery.of(context).size.width / 600,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                    (BuildContext context, int index) {
+                  return new GridView.count(
                     childAspectRatio:MediaQuery.of(context).size.width / 500,
-                    padding: const EdgeInsets.all(10.0),
+                    padding: const EdgeInsets.only(left: 20.0, right: 20.0, bottom: 40.0),
                     crossAxisCount: 2,
                     primary: true,
                     crossAxisSpacing: 10,
                     mainAxisSpacing: 10,
                     scrollDirection: Axis.vertical,
                     children: <Widget>[
-                    _cardsFiles('Bussines', 52, 0xFF151528, 0xFF38A777),
-                    _cardsFiles('Documents', 18, 0xFF151528, 0xFFFCEA4A),
-                    _cardsFiles('Freelancing', 20, 0xFF151528, 0xFFF788CA),
-                    _cardsFiles('Photos', 220, 0xFF151528, 0xFF4093FB),
-                    _cardsFiles('Tutorials', 05, 0xFF151528, 0xFFB07DFD),
-                    _cardsFiles('Videos', 120, 0xFF151528, 0xFFFA665C),
+                      _cardsFiles('Bussines', 52, 0xFF151528, 0xFF47CD91),
+                      _cardsFiles('Documents', 18, 0xFF151528, 0xFFF8A603),
+                      _cardsFiles('Freelancing', 20, 0xFF151528, 0xFFF9936D),
+                      _cardsFiles('Photos', 220, 0xFF151528, 0xFF42C4FA),
+                      _cardsFiles('Tutorials', 05, 0xFF151528, 0xFF6C5EFD),
+                      _cardsFiles('Videos', 120, 0xFF151528, 0xFFF27323),
                     ],
-                  ),
-                ),
-              ],
+                  );
+                },
+                childCount: 1,
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
-  
+
   _cardsFiles(String file, int count, int colorCard, int colorsIcons) {
     return Container(
       width: MediaQuery.of(context).size.width,
       height: 30.0,
       padding: const EdgeInsets.only(top: 20.0),
-
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(20),
-        color: Colors.grey[200],
+        color: Color(0xFFF6F5FB),
       ),
       child: Column(
         children: <Widget>[
@@ -153,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
                         borderRadius: BorderRadius.circular(25),
 //                        color: Color(0xFF151528)
                         color: Color(colorCard)
-                  ),
+                    ),
                     child: Icon(
                       Icons.folder, color: Color(colorsIcons),
                       size: 45,
